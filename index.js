5af2e7ae9cd7582cc758dbacc505db60291ec2a7
Mahin Anowar <mahinanowar479@gmail.com>
2025-12-05 14:37:28 +0600

    crud operations added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -50,6 +50,54 @@ async function run() {
 
     const foodCollection = client.db('food-sharing').collection('foods');
     const foodRequestCollection = client.db('food-sharing').collection('food-requests'); // Assuming 'food-requests'
+
+    // Food API endpoints
+    app.post('/add-food', verifyToken, async (req, res) => {
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    });
+
+    app.get('/available-foods', async (req, res) => {
+      const { search } = req.query;
+      let query = { foodStatus: 'available' };
+      if (search) {
+        query.foodName = { $regex: search, $options: 'i' };
+      }
+      const result = await foodCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get('/manage-foods/:email', verifyToken, async (req, res) => {
+      const tokenEmail = req.params.email;
+      if (tokenEmail !== req.user.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      const query = { 'donator.email': tokenEmail };
+      const result = await foodCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/food/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put('/food/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const food = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...food,
+        },
+      };
+      const result = await foodCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
